863dfaa0f2c403bb85aa55cd4144706c5e0c0d2a
Jogan Daniel A <jogan.d@tvsnext.com>
2023-07-12 11:01:32 +0530

    [jogan] Add : Folder Structure created

---
+++ LineComparison/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LineComparison
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to line comparison problem");
+        }
+    }
+}
